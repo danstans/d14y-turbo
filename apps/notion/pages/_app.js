75ead882fea1d1daeb087df4ae1fa47c08fbb3c6
Daniel Stansberry <daniel@capablehealth.com>
2022-05-20 11:57:12 -0700

    Remove notion site.

--- apps/notion/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import 'react-notion-x/src/styles.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
